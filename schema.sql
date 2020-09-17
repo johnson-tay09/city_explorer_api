e4a3cac0aceb2058b35d13d8e98ef77e2b0252a3
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2020-09-17 13:33:30 -0700

    fixed errors

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS cities;
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
-  city_name VARCHAR(255),
-  latitude VARCHAR(255),
-  longitude VARCHAR(255)
+  formatted_query VARCHAR(255),
+  latitude decimal,
+  longitude decimal
 )

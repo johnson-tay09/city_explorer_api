f5b33dc781bd5e688e24ef195752537900ba739b
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2020-09-17 20:19:19 -0700

    weather sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 
-DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS cities, weather;
 
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
@@ -7,4 +7,14 @@ CREATE TABLE cities (
   formatted_query VARCHAR(255),
   latitude decimal,
   longitude decimal
-)
+);
+
+
+CREATE TABLE weather (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  weather_time VARCHAR(10),
+  forecast VARCHAR(255)
+);
+
+INSERT INTO weatherdata (search_query, forecast, time) VALUES ('seattle', 'awesome', '9/14/2010');
